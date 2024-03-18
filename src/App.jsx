2dadddf79aac20a06e5cb722ad3630078ab68561
Aleksandr2905 <natalukha@ukr.net>
2024-03-18 20:17:35 +0200

    added global style fonts icons images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 style={{ fontFamily: "Right Grotesk", color: "white" }}>
+        Vite + React
+      </h1>
     </>
   );
 }
---
+++ src/stylesheet/theme.js
@@ -0,0 +1,25 @@
+export const theme = {
+  color: {
+    bgPrimary: "#181818",
+    bgSecondary: "#1e1e1e",
+    bgBtnPrimary: "rgba(30, 30, 30, 0.1)",
+    bgBtnSecondary: "rgba(255, 255, 255, 0.1)",
+    accent: "#dc3b5a",
+    textPrimary: "#fff",
+    textSecondary: "#1e1e1e",
+  },
+  fonts: {
+    grotesk: "Right Grotesk",
+    messinaReg: "Messina Sans Mono Regular",
+    messinaBold: "Messina Sans Mono Semi Bold",
+    biro: "Biro Script Plus",
+    biroUS: "Biro Script Plus US",
+  },
+  breakpoints: {
+    mobileMin: "360px",
+    mobileMax: "480px",
+    tablet: "768px",
+    desktop: "1280px",
+    desktopMax: "1440px",
+  },
+};
