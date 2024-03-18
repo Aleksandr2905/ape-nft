9cae0a3e86e01ec4ef0fa6fa03498702984b59f4
Aleksandr2905 <natalukha@ukr.net>
2024-03-18 21:06:41 +0200

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import About from "./components/About/About";
+import Arts from "./components/Arts/Arts";
+import Faq from "./components/Faq/Faq";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import MindMap from "./components/MindMap/MindMap";
+import Mint from "./components/Mint/Mint";
+
 function App() {
   return (
     <>
-      <h1 style={{ fontFamily: "Right Grotesk", color: "white" }}>
-        Vite + React
-      </h1>
+      <Header />
+      <Hero />
+      <About />
+      <MindMap />
+      <Faq />
+      <Arts />
+      <Mint />
+      <Footer />
     </>
   );
 }
