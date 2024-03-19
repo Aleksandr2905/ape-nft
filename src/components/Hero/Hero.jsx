570359ca0ead67c5f6afa5d4d293a80a7b3b1571
Aleksandr2905 <natalukha@ukr.net>
2024-03-19 22:02:23 +0200

    added hero mobile

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import { useScreenWidth } from "../../hooks/useScreenWidth";
+import * as s from "./Hero.styled";
+import { heroImageAdaptive } from "../../data/heroImage";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const screenWidth = useScreenWidth();
+  const [imageAdaptive, setImageAdaptive] = useState(
+    heroImageAdaptive[screenWidth]
+  );
+
+  useEffect(() => {
+    setImageAdaptive(heroImageAdaptive[screenWidth]);
+  }, [screenWidth]);
+
+  return (
+    <s.SectionWrapper>
+      <s.Container>
+        <s.AccentTitle>diD yOu seE iT ?</s.AccentTitle>
+        <s.MainTitle>YACHT APES</s.MainTitle>
+        <s.AccentTitle>Apes aRe eveRywhere</s.AccentTitle>
+        <s.Image style={{ width: "100%" }}>
+          <source
+            srcSet={`${imageAdaptive.oneWebpX} 1x, ${imageAdaptive.twoWebpX} 2x`}
+          />
+          <source
+            srcSet={`${imageAdaptive.oneX} 1x, ${imageAdaptive.twoX} 2x`}
+          />
+          <img src={imageAdaptive.oneX} alt="apes" />
+        </s.Image>
+        <s.Button href="#mint">MEET APES</s.Button>
+        <s.Text>
+          Yacht Ape is a collection of unique digital apes that you can own in
+          NFT format
+        </s.Text>
+      </s.Container>
+    </s.SectionWrapper>
+  );
 };
 
 export default Hero;
---
+++ src/components/Hero/Hero.styled.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import { theme } from "../../stylesheet/theme";
+
+export const SectionWrapper = styled.section`
+  margin: 0 auto;
+  padding: 0 8px;
+`;
+
+export const Container = styled.div`
+  min-width: 360px;
+  max-width: 480px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 36px 64px 19px;
+  border-radius: 12px;
+  text-align: center;
+  /* margin-top: 60px; */
+  background-color: ${theme.color.accent};
+  color: ${theme.color.textSecondary};
+`;
+
+export const AccentTitle = styled.p`
+  font-family: ${theme.fonts.biro};
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.69;
+  /* color: ${theme.color.textSecondary}; */
+`;
+
+export const MainTitle = styled.h1`
+  font-family: ${theme.fonts.grotesk};
+  font-size: 44px;
+  line-height: 1;
+  letter-spacing: 0.01em;
+  margin-bottom: 8px;
+  /* color: ${theme.color.textSecondary}; */
+`;
+
+export const Image = styled.picture`
+  width: 216px;
+  /* height: 284px; */
+`;
+
+export const Button = styled.a`
+  border-radius: 8px;
+  padding: 10px 70px 12px 70px;
+  margin-bottom: 12px;
+  width: 100%;
+  height: 41px;
+  transition: all 0.3s ease;
+  backdrop-filter: blur(12px);
+  background-color: ${theme.color.bgBtnPrimary};
+
+  &:hover {
+    color: ${theme.color.textPrimary};
+  }
+`;
+
+export const Text = styled.p`
+  font-family: ${theme.fonts.messinaReg};
+  font-size: 12px;
+  line-height: 1.17;
+  text-transform: uppercase;
+  /* text-align: center; */
+  /* color: ${theme.color.textSecondary}; */
+`;
