744f6ea4365fae95cfaf60a90189d0a9e89ff805
Aleksandr2905 <natalukha@ukr.net>
2024-03-25 16:19:25 +0200

    added meta tegs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import About from "./components/About/About";
 import Arts from "./components/Arts/Arts";
 import Faq from "./components/Faq/Faq";
 import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import MindMap from "./components/MindMap/MindMap";
 import Mint from "./components/Mint/Mint";
